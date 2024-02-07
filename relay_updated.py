b221b2d6bbd8b6c709874929c313ac873eb6ca78
Smit Patel <142579161+SmitPatel-yuri@users.noreply.github.com>
2024-02-07 15:14:17 +0100

    Add files via upload

--- relay_updated.py
+++ relay_updated.py
@@ -8,7 +8,7 @@ valve3 = 22 #Change to the actual GPIO pin number
 valve4 = 10 #Change to the actual GPIO pin number
 valve5 = 9 #Change to the actual GPIO pin number
 
-# Define the valve number and state of the valves (0 for NO-Open, 1 for NC-Closed)
+# Define the valve number and state of the valves (0 for not active, 1 for active, valves are NC type so not active means valve closed and no power supply)
 def valve_set(num):
     # Setup GPIO
     GPIO.setmode(GPIO.BCM)
@@ -23,86 +23,71 @@ def valve_set(num):
     #num = str(num)
     #state = str(state)
     if num == 1121314151:
-        print("Valve 1,2,3,4,5 are close.")
+        # Configuration for Flow-rate test and filling the fluidic loop with water
+        print("Valve 1,2,3,4,5 are active.")
         GPIO.output(valve1, GPIO.LOW)
         GPIO.output(valve2, GPIO.LOW)
         GPIO.output(valve3, GPIO.LOW)
         GPIO.output(valve4, GPIO.LOW)
         GPIO.output(valve5, GPIO.LOW)
-    elif num == 1020304151:
-        # Max. pressure test air in cw direction
-        print("Valve 1,2,3 are open and valve 4,5 are close.")
-        GPIO.output(valve1, GPIO.HIGH)
-        GPIO.output(valve2, GPIO.HIGH)
-        GPIO.output(valve3, GPIO.HIGH)
-        GPIO.output(valve4, GPIO.LOW)
-        GPIO.output(valve5, GPIO.LOW)
-    elif num == 1121304050:
-        #Min. pressure(vacuum) test air in cw direction
-        print("Valve 3,4,5 are open and valve 1,2 are close.")
-        GPIO.output(valve1, GPIO.LOW)
-        GPIO.output(valve2, GPIO.LOW)
-        GPIO.output(valve3, GPIO.HIGH)
-        GPIO.output(valve4, GPIO.HIGH)
-        GPIO.output(valve5, GPIO.HIGH)
     elif num == 1021314050:
-        #Max. pressure test air in ccw direction
-        print("Valve 1,4,5 are open and valve 2,3 are close.")
+        # Configuartion for max. pressure test air in cw direction min pressure(vacuum) test air in ccw direction 
+        print("Valve 1,4,5 are not active and valve 2,3 are active.")
         GPIO.output(valve1, GPIO.HIGH)
         GPIO.output(valve2, GPIO.LOW)
         GPIO.output(valve3, GPIO.LOW)
         GPIO.output(valve4, GPIO.HIGH)
         GPIO.output(valve5, GPIO.HIGH)
+    elif num == 1020304150:
+        # Configuartion for min. pressure(vacuum) test air in cw direction & max. pressure test air in ccw direction
+        print("Valve 1,2,3,5 are not active and valve 4 is active.")
+        GPIO.output(valve1, GPIO.HIGH)
+        GPIO.output(valve2, GPIO.HIGH)
+        GPIO.output(valve3, GPIO.HIGH)
+        GPIO.output(valve4, GPIO.LOW)
+        GPIO.output(valve5, GPIO.HIGH)
+    elif num == 1021304150:
+        # Configuartion for air flush between cw-ccw air tests
+        print("Valve 1,3,5 are not active and valve 2,4 are active.")
+        GPIO.output(valve1, GPIO.HIGH)
+        GPIO.output(valve2, GPIO.LOW)
+        GPIO.output(valve3, GPIO.HIGH)
+        GPIO.output(valve4, GPIO.LOW)
+        GPIO.output(valve5, GPIO.HIGH)
+    elif num == 1121314051:
+        # Configuartion for max pressure test water in cw direction 
+        print("Valve 4 is not active and valve 1,2,3,5 are active.")
+        GPIO.output(valve1, GPIO.LOW)
+        GPIO.output(valve2, GPIO.LOW)
+        GPIO.output(valve3, GPIO.LOW)
+        GPIO.output(valve4, GPIO.HIGH)
+        GPIO.output(valve5, GPIO.LOW)
     elif num == 1120304151:
-        # Min pressure(vacuum) test air in ccw direction 
-        print("Valve 2,3 are open and valve 1,4,5 are close.")
+        # Configuartion for max pressure test water in ccw direction
+        print("Valve 2,3 are not active and valve 1,4,5 are active.")
         GPIO.output(valve1, GPIO.LOW)
         GPIO.output(valve2, GPIO.HIGH)
         GPIO.output(valve3, GPIO.HIGH)
         GPIO.output(valve4, GPIO.LOW)
         GPIO.output(valve5, GPIO.LOW)
-    elif num == 1120314051:
-        # For all fluidic line open (flowrate and filling for both directions)
-        print("Valve 2,4 are open and valve 1,3,5 are close.")
+    elif num == 1121304150:
+        # Configuartion for flushing water with pump running in ccw direction
+        print("Valve 3,5 are not active and valve 1,2,4 are active.")
         GPIO.output(valve1, GPIO.LOW)
-        GPIO.output(valve2, GPIO.HIGH)
-        GPIO.output(valve3, GPIO.LOW)
-        GPIO.output(valve4, GPIO.HIGH)
-        GPIO.output(valve5, GPIO.LOW)
-    elif num == 1120304051:
-        # For all fluidic line open (flowrate and filling for both directions)
-        print("Valve 2,3,4 are open and valve 1,5 are close.")
-        GPIO.output(valve1, GPIO.LOW)
-        GPIO.output(valve2, GPIO.HIGH)
-        GPIO.output(valve3, GPIO.LOW)
-        GPIO.output(valve4, GPIO.HIGH)
-        GPIO.output(valve5, GPIO.LOW)
-    elif num == 1120314151:
-        # Max. pressure and leak test water cw direction
-        print("Valve 2 is open and valve 1,3,4,5 are close.")
-        GPIO.output(valve1, GPIO.LOW)
-        GPIO.output(valve2, GPIO.HIGH)
+        GPIO.output(valve2, GPIO.LOW)
+        GPIO.output(valve3, GPIO.HIGH)
+        GPIO.output(valve4, GPIO.LOW)
+        GPIO.output(valve5, GPIO.HIGH)
+    elif num == 1021314151:
+        # Configuartion for flushing water with pump running in cw direction
+        print("Valve 1 is not active and valve 2,3,4,5 are active.")
+        GPIO.output(valve1, GPIO.HIGH)
+        GPIO.output(valve2, GPIO.LOW)
         GPIO.output(valve3, GPIO.LOW)
         GPIO.output(valve4, GPIO.LOW)
-        GPIO.output(valve5, GPIO.LOW)
-    elif num == 1021314051:
-        # Max. pressure and leak test water ccw direction
-        print("Valve 1,4 are open and valve 2,3,5 are close.")
-        GPIO.output(valve1, GPIO.HIGH)
-        GPIO.output(valve2, GPIO.LOW)
-        GPIO.output(valve3, GPIO.LOW)
-        GPIO.output(valve4, GPIO.HIGH)
-        GPIO.output(valve5, GPIO.LOW)
-    elif num == 1020314050:
-        # Prepare for the next test fill with air
-        print("Valve 1,2,4,5 are open and valve 3 is close.")
-        GPIO.output(valve1, GPIO.HIGH)
-        GPIO.output(valve2, GPIO.LOW)
-        GPIO.output(valve3, GPIO.LOW)
-        GPIO.output(valve4, GPIO.HIGH)
-        GPIO.output(valve5, GPIO.LOW)        
+        GPIO.output(valve5, GPIO.LOW)     
     elif num == 1020304050:
-        print("Valve 1,2,3,4,5 are open.")
+        print("Valve 1,2,3,4,5 are not active.")
         GPIO.output(valve1, GPIO.HIGH)
         GPIO.output(valve2, GPIO.HIGH)
         GPIO.output(valve3, GPIO.HIGH)
@@ -110,37 +95,28 @@ def valve_set(num):
         GPIO.output(valve5, GPIO.HIGH)
     else:
         print("Invalid valve setting specified.")
-""" valve_set(102030)
+""" valve_set(1021314050)
 time.sleep(3)
-valve_set(112131)
+valve_set(1020304150)
 time.sleep(3)
-valve_set(112030)
+valve_set(1021304150)
 time.sleep(3)
-valve_set(102130)
+valve_set(1121314151)
 time.sleep(3)
-valve_set(102031)
+valve_set(1121314051)
 time.sleep(3)
-valve_set(112130)
+valve_set(1120304151)
 time.sleep(3)
-valve_set(102131)
+valve_set(1121304150)
 time.sleep(3)
-valve_set(102030)
+valve_set(1021314151)
 time.sleep(3)
-valve_set(112031)
+valve_set(1020304050)
 time.sleep(3)
-valve_set(112131)
-time.sleep(3) """
-
-""" GPIO.output(valve1, GPIO.HIGH)
-print("Valve 1,2,3 are not open.")
-time.sleep(15)
-GPIO.output(valve1, GPIO.LOW)
-print("Valve 1,2,3 are open.")
-time.sleep(15)
-GPIO.output(valve1, GPIO.HIGH)
+valve_set(1121314150)
 time.sleep(3)
-GPIO.output(valve1, GPIO.LOW)
-time.sleep(3) """
-#valve_set(102030)
-#time.sleep(1)
-#GPIO.cleanup()
+valve_set(1020304050)
+time.sleep(3)
+valve_set(1121314151)
+time.sleep(3)
+GPIO.cleanup() """
